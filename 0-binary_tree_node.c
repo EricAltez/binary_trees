8b442e4a8b8259fa98970eea2624998366fe757b
EricAltez <ericaltez1111@gmail.com>
2022-03-23 03:42:08 -0300

    problems vscode

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ *
+ *
+ *
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+{
+    binary_tree_t *new_node = *parent;
+    
+}
